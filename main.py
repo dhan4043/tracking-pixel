d85153770164461c9dab5998f9f967f25f4c8a4b
dhan4043 <dhan4043@gmail.com>
2025-07-28 14:50:07 -0400

    Finalizing

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ app = Flask(__name__)
 def tracking_pixel():
     # File path and name for 1 x 1 pixel. Must be an absolute path to pixel
     filename = './static/img/pixel.png'
-    # Log the User-Agent String
     user_agent = request.headers.get('User-Agent')
-    # Get the current time of request and format time into readable format
     current_time = datetime.datetime.now()
     timestamp = datetime.datetime.strftime(current_time, '%Y-%m-%d %H:%M:%S')
-
-    # Log the IP address of requester
     get_ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0]
 
     # Lookup Geolocation of IP Address
@@ -27,8 +23,6 @@ def tracking_pixel():
     # Write log to hardcoded path. Must be an absolute path to the log file
     with open('./logs.txt', 'w') as file:
         file.write(log_entry)
-
-    # Write log to console
     print(log_entry)
 
     # Serve a transparent pixel image when navigating to .../image URL. "image/png" displays the image in PNG format
